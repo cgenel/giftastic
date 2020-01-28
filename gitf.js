47f5918a7a38fdaca797c8332cd4ae7fc25ae848
cgenel <genel661@gmail.com>
2020-01-28 13:55:41 -0800

    animals options array created

---
+++ gitf.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+  // animals search buttons array that will populate into the index.html
+  var animals = [
+    "dog", "cat", "rabbit", "hamster", "skunk", "goldfish", "bird", "ferret", "turtle", "sugar glider", "chinchilla",
+    "hedgehog", "hermit crab", "gerbill", "pygmy goat", "chicken", "capybara", "teacup pif=g", "serval", "salamander", "frog"
+  ];
+
+
+});
